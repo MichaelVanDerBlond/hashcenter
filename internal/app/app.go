15eec1bb3094eb4347058ba9854d700f2968b410
root <root@vdb-srv.ru>
2026-07-16 15:10:07 +0300

    R0.2.1: bootstrap application framework

---
+++ internal/app/app.go
@@ -0,0 +1,7 @@
+package app
+
+import "github.com/MichaelVanDerBlond/hashcenter/internal/cli"
+
+func Run() error {
+	return cli.Execute()
+}
---
+++ internal/version/version.go
@@ -0,0 +1,6 @@
+package version
+
+const (
+	Name    = "HashCenter"
+	Version = "0.2.0-dev"
+)
