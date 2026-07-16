3d7c0a6395c17019cba25afa49ffeebe7a81b5ed
root <root@vdb-srv.ru>
2026-07-16 15:23:07 +0300

    R0.3.1: doctor uses system inventory

--- internal/cli/doctor.go
+++ internal/cli/doctor.go
@@ -3,28 +3,31 @@ package cli
 import (
 	"fmt"
 
-	"github.com/MichaelVanDerBlond/hashcenter/internal/doctor"
-	"github.com/MichaelVanDerBlond/hashcenter/internal/doctor/checks"
+	"github.com/MichaelVanDerBlond/hashcenter/internal/system"
 )
 
 func Doctor() error {
-	d := doctor.Doctor{
-		Checks: []doctor.Check{
-			checks.OS{},
-			checks.Kernel{},
-			checks.Hashcat{},
-		},
+	info, err := system.Collect()
+	if err != nil {
+		return err
 	}
 
-	results := d.Run()
+	fmt.Printf("%-2s %-20s %s\n", "✔", "Operating System", info.OS)
+	fmt.Printf("%-2s %-20s %s\n", "✔", "Kernel", info.Kernel)
 
-	for _, r := range results {
-		fmt.Printf("%-2s %-20s %s\n",
-			r.Status.Icon(),
-			r.Name,
-			r.Message,
-		)
+	if info.Hashcat == "not installed" {
+		fmt.Printf("%-2s %-20s %s\n", "!", "Hashcat", info.Hashcat)
+	} else {
+		fmt.Printf("%-2s %-20s %s\n", "✔", "Hashcat", info.Hashcat)
 	}
+
+	fmt.Printf("%-2s %-20s %s\n", "✔", "CPU", info.CPU.Model)
+	fmt.Printf("%-2s %-20s %d cores\n", "✔", "CPU Cores", info.CPU.Cores)
+	fmt.Printf("%-2s %-20s %.2f GB\n",
+		"✔",
+		"Memory",
+		float64(info.Memory.Total)/(1024*1024*1024),
+	)
 
 	return nil
 }
