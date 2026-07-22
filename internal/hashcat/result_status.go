52185e9af418fe6d107ea681938139304620e38c
MichaelVanDerBlond <ub6gtx@gmail.com>
2026-07-22 16:29:19 +0300

    hashcat: simplify result status helpers

--- internal/hashcat/result_status.go
+++ internal/hashcat/result_status.go
@@ -1,37 +1,17 @@
 package hashcat
 
 func (r *Result) Success() bool {
-
-	if r == nil {
-		return false
-	}
-
-	return r.ExitCode == 0
+	return r != nil && r.ExitCode == 0
 }
 
 func (r *Result) Cracked() bool {
-
-	if r == nil {
-		return false
-	}
-
-	return r.State == "Cracked"
+	return r != nil && r.State == "Cracked"
 }
 
 func (r *Result) Exhausted() bool {
-
-	if r == nil {
-		return false
-	}
-
-	return r.State == "Exhausted"
+	return r != nil && r.State == "Exhausted"
 }
 
 func (r *Result) Running() bool {
-
-	if r == nil {
-		return false
-	}
-
-	return r.State == "Running"
+	return r != nil && r.State == "Running"
 }
