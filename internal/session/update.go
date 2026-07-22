14dedb79bd3034d2c61763fd99abbae24e2f50f0
MichaelVanDerBlond <ub6gtx@gmail.com>
2026-07-22 16:39:12 +0300

    session: reset state on start

--- internal/session/update.go
+++ internal/session/update.go
@@ -3,21 +3,36 @@ package session
 import "time"
 
 func (s *Session) Start() {
+	if s == nil {
+		return
+	}
+
 	s.State = Running
 	s.Started = time.Now()
+	s.Finished = time.Time{}
+	s.Error = ""
 }
 
 func (s *Session) Finish() {
+	if s == nil {
+		return
+	}
+
 	s.State = Finished
 	s.Finished = time.Now()
 }
 
 func (s *Session) Fail(err error) {
+	if s == nil {
+		return
+	}
 
 	s.State = Failed
 	s.Finished = time.Now()
 
 	if err != nil {
 		s.Error = err.Error()
+	} else {
+		s.Error = ""
 	}
 }
