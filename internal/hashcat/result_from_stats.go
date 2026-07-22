428012c343f61080fbe121700595c6d9007415a0
MichaelVanDerBlond <ub6gtx@gmail.com>
2026-07-22 16:29:52 +0300

    hashcat: populate result timing fields

--- internal/hashcat/result_from_stats.go
+++ internal/hashcat/result_from_stats.go
@@ -3,14 +3,17 @@ package hashcat
 import "time"
 
 func NewResult(stats Stats, started time.Time) *Result {
+	finished := time.Now()
 
 	return &Result{
+		Started:   started,
+		Finished:  finished,
+		Duration:  finished.Sub(started),
 		State:     stats.State,
 		Speed:     stats.Speed,
 		SpeedHPS:  stats.SpeedHPS,
 		Progress:  stats.Progress,
 		Recovered: stats.Recovered,
 		ETA:       stats.ETA,
-		Duration:  time.Since(started),
 	}
 }
