504ed8818a6a65e72e4ab39faf98e8e90b2c3130
root <root@vdb-srv.ru>
2026-07-22 15:44:27 +0300

    refactor(app): introduce application object

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,25 @@
 package app
 
-import "github.com/MichaelVanDerBlond/hashcenter/internal/cli"
+import (
+	"context"
+
+	"github.com/MichaelVanDerBlond/hashcenter/internal/cli"
+)
+
+type App struct {
+	ctx context.Context
+}
+
+func New() *App {
+	return &App{
+		ctx: context.Background(),
+	}
+}
+
+func (a *App) Run() error {
+	return cli.Execute()
+}
 
 func Run() error {
-	return cli.Execute()
+	return New().Run()
 }
