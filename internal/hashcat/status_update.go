4d8b76591719c576aeed20c749dda4767c0ab579
MichaelVanDerBlond <ub6gtx@gmail.com>
2026-07-22 16:35:09 +0300

    hashcat: deduplicate status update methods

--- internal/hashcat/status_update.go
+++ internal/hashcat/status_update.go
@@ -6,7 +6,6 @@ import (
 )
 
 func value(line string) string {
-
 	if i := strings.Index(line, ":"); i >= 0 {
 		return strings.TrimSpace(line[i+1:])
 	}
@@ -14,47 +13,30 @@ func value(line string) string {
 	return strings.TrimSpace(line)
 }
 
-func (s *Status) SetState(v string) {
-
+func (s *Status) set(update *string, v string) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	s.State = value(v)
+	*update = value(v)
 	s.LastUpdate = time.Now()
+}
+
+func (s *Status) SetState(v string) {
+	s.set(&s.State, v)
 }
 
 func (s *Status) SetSpeed(v string) {
-
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	s.Speed = value(v)
-	s.LastUpdate = time.Now()
+	s.set(&s.Speed, v)
 }
 
 func (s *Status) SetProgress(v string) {
-
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	s.Progress = value(v)
-	s.LastUpdate = time.Now()
+	s.set(&s.Progress, v)
 }
 
 func (s *Status) SetRecovered(v string) {
-
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	s.Recovered = value(v)
-	s.LastUpdate = time.Now()
+	s.set(&s.Recovered, v)
 }
 
 func (s *Status) SetETA(v string) {
-
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	s.ETA = value(v)
-	s.LastUpdate = time.Now()
+	s.set(&s.ETA, v)
 }
