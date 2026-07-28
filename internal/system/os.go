cbd422a2a509d3a1955a20b18057782e4a17eea2
MichaelVanDerBlond <ub6gtx@gmail.com>
2026-07-28 15:37:53 +0300

    system: add fallback OS name

--- internal/system/os.go
+++ internal/system/os.go
@@ -2,21 +2,26 @@ package system
 
 import (
 	"os"
+	"runtime"
 	"strings"
 )
 
 func collectOS(info *Info) error {
+
 	data, err := os.ReadFile("/etc/os-release")
 	if err != nil {
-		return err
+		info.OS = runtime.GOOS
+		return nil
 	}
 
 	for _, line := range strings.Split(string(data), "\n") {
 		if strings.HasPrefix(line, "PRETTY_NAME=") {
 			info.OS = strings.Trim(strings.TrimPrefix(line, "PRETTY_NAME="), "\"")
-			break
+			return nil
 		}
 	}
+
+	info.OS = runtime.GOOS
 
 	return nil
 }
