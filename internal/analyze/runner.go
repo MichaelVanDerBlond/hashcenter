af9b6a8792b1d3c753cb14442ffb6053d119787e
MichaelVanDerBlond <ub6gtx@gmail.com>
2026-07-28 15:39:45 +0300

    analyze: simplify command runner

--- internal/analyze/runner.go
+++ internal/analyze/runner.go
@@ -1,28 +1,11 @@
 package analyze
 
 import (
-	"bytes"
 	"os/exec"
-	"strings"
 )
 
 func run(name string, args ...string) ([]byte, error) {
-
-	cmd := exec.Command(name, args...)
-
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	err := cmd.Run()
-
-	if stdout.Len() > 0 {
-		return stdout.Bytes(), err
-	}
-
-	return []byte(strings.TrimSpace(stderr.String())), err
+	return exec.Command(name, args...).CombinedOutput()
 }
 
 func exists(name string) bool {
