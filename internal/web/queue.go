d2fa6b1cbd52b61c5fe8fe9ac642db41782e5d0b
MichaelVanDerBlond <ub6gtx@gmail.com>
2026-07-28 16:02:31 +0300

    web: synchronize queue state updates

--- internal/web/queue.go
+++ internal/web/queue.go
@@ -20,39 +20,37 @@ var (
 )
 
 func startQueue() {
-
 	once.Do(func() {
-
 		queue = make(chan queuedAttack, 128)
 
 		go func() {
-
 			for item := range queue {
-
+				queueMu.Lock()
 				item.meta.State = "running"
 				item.meta.Started = time.Now()
+				queueMu.Unlock()
 
 				err := attack.Execute(context.Background(), item.job)
 
+				queueMu.Lock()
 				item.meta.Finished = time.Now()
 
 				if err != nil {
-
 					item.meta.State = "failed"
+					queueMu.Unlock()
 
 					log.Println(err)
-
 					continue
 				}
 
 				item.meta.State = "finished"
+				queueMu.Unlock()
 			}
 		}()
 	})
 }
 
 func enqueue(job attack.Job) {
-
 	startQueue()
 
 	meta := addQueueJob(
