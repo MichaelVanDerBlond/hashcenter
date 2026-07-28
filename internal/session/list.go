24a569ae77c2485cbb5b6a8711609ad634c3df29
MichaelVanDerBlond <ub6gtx@gmail.com>
2026-07-28 15:49:21 +0300

    session: return sessions in stable order

--- internal/session/list.go
+++ internal/session/list.go
@@ -1,5 +1,7 @@
 package session
 
+import "sort"
+
 func (m *Manager) List() []*Runtime {
 	if m == nil {
 		return nil
@@ -13,6 +15,16 @@ func (m *Manager) List() []*Runtime {
 	for _, runtime := range m.sessions {
 		list = append(list, runtime)
 	}
+
+	sort.Slice(list, func(i, j int) bool {
+		if list[i] == nil || list[i].Session == nil {
+			return false
+		}
+		if list[j] == nil || list[j].Session == nil {
+			return true
+		}
+		return list[i].Session.ID < list[j].Session.ID
+	})
 
 	return list
 }
