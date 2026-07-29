1769c7268a251f99d165515bac7ae4501212b18b
MichaelVanDerBlond <ub6gtx@gmail.com>
2026-07-29 11:48:58 +0300

    task: add attack task constructor

--- internal/task/new.go
+++ internal/task/new.go
@@ -3,19 +3,40 @@ package task
 import (
 	"time"
 
+	"github.com/MichaelVanDerBlond/hashcenter/internal/attack"
 	"github.com/google/uuid"
 )
+
+func NewAttack(sessionID string, job attack.Job) *Task {
+
+	return &Task{
+		ID:        uuid.NewString(),
+		SessionID: sessionID,
+		Type:      Attack,
+		State:     Queued,
+		Created:   time.Now(),
+
+		HashFile:   job.HashFile,
+		Dictionary: job.Dictionary,
+
+		HashMode:   job.HashMode,
+		AttackMode: job.AttackMode,
+
+		Rule:        job.Rule,
+		Mask:        job.Mask,
+		Device:      job.Device,
+		Workload:    job.Workload,
+		SessionName: job.SessionName,
+	}
+}
 
 func New(sessionID string, typ Type) *Task {
 
 	return &Task{
-		ID:         uuid.NewString(),
-		SessionID:  sessionID,
-		Type:       typ,
-		State:      Queued,
-		Created:    time.Now(),
-		HashMode:   22000,
-		AttackMode: 0,
-		Workload:   3,
+		ID:        uuid.NewString(),
+		SessionID: sessionID,
+		Type:      typ,
+		State:     Queued,
+		Created:   time.Now(),
 	}
 }
