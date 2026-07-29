ccd255e8bf1c485c59efa9bbea0fb2037cc7ec2a
MichaelVanDerBlond <ub6gtx@gmail.com>
2026-07-29 11:48:33 +0300

    task: initialize default attack settings

--- internal/task/new.go
+++ internal/task/new.go
@@ -9,10 +9,13 @@ import (
 func New(sessionID string, typ Type) *Task {
 
 	return &Task{
-		ID:        uuid.NewString(),
-		SessionID: sessionID,
-		Type:      typ,
-		State:     Queued,
-		Created:   time.Now(),
+		ID:         uuid.NewString(),
+		SessionID:  sessionID,
+		Type:       typ,
+		State:      Queued,
+		Created:    time.Now(),
+		HashMode:   22000,
+		AttackMode: 0,
+		Workload:   3,
 	}
 }
